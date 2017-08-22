4c23f3bec49d8b2b543ecb4820d1e26d5bcd8a2f
User <yasser.morteza@gmail.com>
2017-08-22 11:26:55 +0200

    npm packages were installed 'node-sass, jshint, uglify-js, browserify, ...' some changes were applied in css code and html index

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-function (){
+var simulateProgress = require('simulate-progress');
 
+/* jshint browserify: true */
+
+var div = document.getElementsByTagName('div'),
+    loader = div[0],
+    progress = div[1];
+
+function progressCallback (percent){
 
 }
+
+function finishCallback(){
+
+ loader.addClass('finished');
+
+}
+
+function startSimulation(){
+
+  loader.removeClass('finished');
+}
+
+simulateProgress(progressCallback, finishCallback);
+
+
